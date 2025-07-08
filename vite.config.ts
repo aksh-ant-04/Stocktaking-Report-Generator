fc61cfc2edc1e4ea01e303d82ea83cdc4934498f
aksh-ant-04 <167125016+aksh-ant-04@users.noreply.github.com>
2025-07-08 16:33:30 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), 
+           VitePWA({
+             manifest: {
+               icons: [
+                 {
+                   src: "icons/icon-192.png",
+                   sizes: "192x192",
+                   type: "image/png"
+                   purpose: "any maskable"
+                 }
+                ] 
+             }
+           })
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
